74730c92cfb0d5191ab133351271908a5a675552
Vahe Sahakyan <vahe@spir.me>
2022-09-30 09:46:03 +0200

    feat: upgrade package/nodejs to v2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+# v2.0.0 (September 30, 2022)
+ * Upgrade package/nodejs module to v2.0
+
 # v1.1.0 (November 28, 2021)
  * First stable version
--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "package" {
   source  = "cloudventure-io/package/nodejs"
-  version = "~> 1.0"
+  version = "~> 2.0"
 
   files = [
     {
